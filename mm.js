55dc9ce919ab649bd81da64f1817410cdf210277
hosannatandi <44102131+hosannatandi@users.noreply.github.com>
2020-11-10 21:56:05 -0500

    Add files via upload

---
+++ mm.js
@@ -0,0 +1,8 @@
+function changeText(obj, text) {obj.innerText = text;}
+//function changeColor(obj) {
+    //if (obj.style.color =="orange") {
+     //   (obj.style.color ='black');
+    //}else{obj.style.color = 'orange';}
+    
+//}
+// for help: https://www.youtube.com/watch?v=mrVkYzMXs1s&ab_channel=CodingwithSara 
